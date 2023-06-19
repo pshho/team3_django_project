50c166996d4766ac54e95a1c163b8238e7f953ac
pshho <awdr498@gmail.com>
2023-06-19 23:57:10 +0900

    v5

--- polls/config/urls.py
+++ polls/config/urls.py
@@ -17,8 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from poll import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('poll/', include('poll.urls'))
--- polls/poll/urls.py
+++ polls/poll/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
-from poll import views
-
-app_name = 'poll'
+from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('board/',views.board, name='board'),
+    path('board/', views.board, name='board'),
 ]
