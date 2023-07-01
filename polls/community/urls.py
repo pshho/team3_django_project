e79e518e4fa9a3ede8cc69395e7c056ffcf493a5
nsw <tmddn3410@naver.com>
2023-07-02 03:16:12 +0900

    승우자유게시판완거래게시판댓글하는중

--- polls/community/urls.py
+++ polls/community/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     path('board_free_recommend/', views.board_free_recommend, name='free_recommend'),  # 자유게시판 추천수 보기
 
     path('<int:free_board_id>/', views.board_free_detail, name='detail'),  # 게시판 상세보기
+
     path('board_free_update/<int:free_board_id>',views.board_free_update, name='update'), # 게시판 수정
+    path('comment_update/<int:comment_id>/', views.comment_update, name='comment_update'),
 
     path('board_free_write/',views.board_free_write, name='write'), # 게시판 작성
     path('comment/<int:free_board_id>/', views.comment_create, name='comment_create'), # 댓글 등록
